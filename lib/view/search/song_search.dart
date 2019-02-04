b87e6103c929b6491f710baf1272ef635835a3ac
CiyLei <903736662@qq.com>
2019-02-04 16:33:17 +0800

    add: 歌曲搜索界面保存历史搜索

--- lib/view/search/song_search.dart
+++ lib/view/search/song_search.dart
@@ -4,6 +4,7 @@ import 'package:flutter_kugou/component/base_state.dart';
 import 'package:flutter_kugou/component/navigator/kugou_navigator.dart';
 import 'package:flutter_kugou/view/search/song_search_bean.dart';
 import 'package:flutter_kugou/view/search/song_search_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SongSearch extends StatefulWidget {
   @override
@@ -15,6 +16,7 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
   FocusNode _focusNode;
   String _search;
   TextEditingController _searchController;
+  List<String> searchHistory;
 
   @override
   void initState() {
@@ -24,6 +26,12 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
     _focusNode = FocusNode();
     _focusNode.addListener(() {
       setState(() {});
+    });
+    searchHistory = [];
+    getSearchHistory().then((val) {
+      setState(() {
+        searchHistory = val;
+      });
     });
   }
 
@@ -59,10 +67,18 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
               builder:
                   (BuildContext context, AsyncSnapshot<SearchBean> snapshot) {
                 return snapshot.data == null
-                    ? Container(
-                        color: Colors.orangeAccent,
-                      )
-                    : _buildSearchList(snapshot);
+                    ? _buildSearchHisotyList(searchHistory, onItemTap: (index) {
+                        searchSong(searchHistory[index]);
+                      }, onClearTap: () {
+                        deleteAllSearchHistory();
+                      }, onItemDeleteTap: (index) {
+                        deleteSearchHistory(searchHistory[index]);
+                      })
+                    : _buildSearchList(
+                        snapshot.data.data.map((val) => val.keyword).toList(),
+                        onItemTap: (index) {
+                        searchSong(snapshot.data.data[index].keyword);
+                      });
               },
             )
           : Container(
@@ -71,13 +87,89 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
     );
   }
 
-  ListView _buildSearchList(AsyncSnapshot<SearchBean> snapshot) {
+  Widget _buildSearchHisotyList(List<String> history,
+      {ValueChanged<int> onItemTap,
+      GestureTapCallback onClearTap,
+      ValueChanged<int> onItemDeleteTap}) {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        return Material(
+            child: InkWell(
+          onTap: () {
+            index == history.length ? onClearTap() : onItemTap(index);
+          },
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 10.0, bottom: 10.0, left: 15.0, right: 15.0),
+                child: (index == history.length)
+                    ? (history.length == 0
+                        ? SizedBox()
+                        : Center(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 10.0, bottom: 10.0),
+                              child: Text(
+                                "清空搜索历史",
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 12.0),
+                              ),
+                            ),
+                          ))
+                    : Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.search,
+                            color: Colors.grey[300],
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Text(history[index],
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 12.0)),
+                          Expanded(
+                            child: SizedBox(),
+                          ),
+                        ],
+                      ),
+              ),
+              index == history.length
+                  ? SizedBox()
+                  : Positioned(
+                      right: 0.0,
+                      top: 0.0,
+                      bottom: 0.0,
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            color: Colors.grey[300],
+                          ),
+                          onPressed: () {
+                            onItemDeleteTap(index);
+                          }),
+                    )
+            ],
+          ),
+        ));
+      },
+      separatorBuilder: (BuildContext context, int index) => Divider(
+            height: 1.0,
+            indent: 15.0,
+          ),
+      itemCount: history.length + 1,
+    );
+  }
+
+  ListView _buildSearchList(List<String> songs, {ValueChanged<int> onItemTap}) {
     return ListView.separated(
       itemBuilder: (BuildContext context, int index) {
         return Material(
           child: InkWell(
             onTap: () {
-              searchSong(snapshot.data.data[index].keyword);
+              onItemTap(index);
             },
             child: Padding(
               padding:
@@ -97,9 +189,7 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
                       style: TextStyle(
                           color: Theme.of(context).primaryColor,
                           fontSize: 12.0),
-                      children: snapshot.data.data[index].keyword
-                          .split(_search)
-                          .map((val) {
+                      children: songs[index].split(_search).map((val) {
                         return val.isEmpty
                             ? TextSpan()
                             : TextSpan(
@@ -108,10 +198,8 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
                                     color: Colors.black, fontSize: 12.0),
                                 children: [
                                     TextSpan(
-                                      text: !snapshot.data.data[index].keyword
-                                                  .endsWith(val) ||
-                                              snapshot.data.data[index].keyword
-                                                  .endsWith(_search)
+                                      text: !songs[index].endsWith(val) ||
+                                              songs[index].endsWith(_search)
                                           ? _search
                                           : "",
                                       style: TextStyle(
@@ -132,10 +220,11 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
             height: 1.0,
             indent: 15.0,
           ),
-      itemCount: snapshot.data.data.length,
+      itemCount: songs.length,
     );
   }
 
+  // 搜索歌曲
   void searchSong(String songName, {isEdit = false}) {
     setState(() {
       _search = songName;
@@ -143,8 +232,42 @@ class _SongSearchState extends BaseState<SongSearch, SongSearchBloc> {
       if (!isEdit) {
         _focusNode.unfocus();
         _searchController.text = _search;
-
+        saveSearchHistory(songName);
       }
+    });
+  }
+
+  // 保存搜索记录
+  void saveSearchHistory(String str) async {
+    deleteSearchHistory(str, inserStr: str);
+  }
+
+  // 读取搜索记录
+  Future<List<String>> getSearchHistory() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getStringList("SearchHistory") ?? [];
+  }
+
+  // 删除某个搜索记录
+  void deleteSearchHistory(String str, {String inserStr}) async {
+    List<String> searchHistory = await getSearchHistory();
+    searchHistory.remove(str);
+    if (inserStr != null && inserStr.isNotEmpty) {
+      searchHistory.insert(0, inserStr);
+    }
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setStringList("SearchHistory", searchHistory);
+    setState(() {
+      this.searchHistory = searchHistory;
+    });
+  }
+
+  // 删除全部的搜索记录
+  void deleteAllSearchHistory() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setStringList("SearchHistory", []);
+    setState(() {
+      this.searchHistory = [];
     });
   }
 
