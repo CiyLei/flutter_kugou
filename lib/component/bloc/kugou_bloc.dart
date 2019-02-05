656e1d71b22271ec95ee23144a34e7b48093ba33
CiyLei <903736662@qq.com>
2019-02-05 16:12:34 +0800

    opt: 播放功能逻辑优化

--- lib/component/bloc/kugou_bloc.dart
+++ lib/component/bloc/kugou_bloc.dart
@@ -20,46 +20,52 @@ class KuGouBloc extends BlocBase {
       _play_index = _plays.indexOf(bean);
     else
       _plays.insert(++_play_index, bean);
-    _play();
+    _playNewSong();
   }
 
   void add(SongInfoBean bean) {
     if (_plays.contains(bean)) _plays.remove(bean);
     _plays.insert((_play_index + 1), bean);
-    _play();
+    _playNewSong();
   }
 
   void playNext() {
-    ++_play_index;
-    if (_play_index >= _plays.length)
-      _play_index = 0;
-    _play();
+    if (_plays.length > 0) {
+      ++_play_index;
+      if (_play_index >= _plays.length)
+        _play_index = 0;
+      _playNewSong();
+    }
   }
 
   void play() {
     if (_playerController != null) {
       _playerController.play();
-      _playerStreamController.add(PlaySongInfoBean(
+      _sendStream(PlaySongInfoBean(
           songInfo: _plays[_play_index],
           duration: _playerController.value.duration,
           position: _playerController.value.position,
           state: 1));
+    } else {
+      _sendStream(null);
     }
   }
 
   void pause() {
     if (_playerController != null) {
       _playerController.pause();
-      _playerStreamController.add(PlaySongInfoBean(
+      _sendStream(PlaySongInfoBean(
           songInfo: _plays[_play_index],
           duration: _playerController.value.duration,
           position: _playerController.value.position,
           state: 0));
+    } else {
+      _sendStream(null);
     }
   }
 
   // 1为播放 0为暂停
-  void _play() {
+  void _playNewSong() {
     if (_playerController != null) {
       _playerController.pause();
       _playerController.dispose();
@@ -68,12 +74,24 @@ class KuGouBloc extends BlocBase {
         VideoPlayerController.network(_plays[_play_index].data.play_url)
           ..initialize();
     _playerController.addListener(() {
-      _playerStreamController.add(PlaySongInfoBean(
+      _sendStream(PlaySongInfoBean(
           songInfo: _plays[_play_index],
           duration: _playerController.value.duration,
           position: _playerController.value.position,));
     });
     _playerController.play();
+  }
+
+  void _sendStream(PlaySongInfoBean bean) {
+    if (bean == null) {
+      _playerStreamController.add(PlaySongInfoBean(
+          songInfo: null,
+          duration: Duration(seconds: 0),
+          position: Duration(seconds: 0),
+          state: 0));
+    } else {
+      _playerStreamController.add(bean);
+    }
   }
 
   @override
