0d1aff9382ea7a1b17c44b4e3e07a32dbba54627
CiyLei <903736662@qq.com>
2019-02-08 18:40:46 +0800

    opt: 解决首页listview和drawer的滑动冲突（仍不完美）

--- lib/component/view/kugou_home_tabbar.dart
+++ lib/component/view/kugou_home_tabbar.dart
@@ -16,6 +16,8 @@ class _KuGouTabBarViewState extends State<KuGouTabBarView> {
   Offset downPoint;
   bool collectionFlag = true;
   bool moveDraw = false;
+  // 是否有滑动冲突
+  bool scrollerNoConflict = false;
 
   @override
   void initState() {
@@ -24,37 +26,47 @@ class _KuGouTabBarViewState extends State<KuGouTabBarView> {
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: (PointerDownEvent event) {
-        collectionFlag = true;
-        downPoint = event.position;
-        moveDraw = false;
+    return NotificationListener(
+      onNotification: (ScrollNotification notification) {
+        scrollerNoConflict = notification.depth == 0 && widget.controller.index == 0;
+        return true;
       },
-      onPointerMove: (PointerMoveEvent event) {
-        if (collectionFlag && event.position.dx > downPoint.dx && widget.controller.index == 0) {
-          setState(() {
-            moveDraw = true;
-          });
-          collectionFlag = false;
-        }
-        if (moveDraw ) {
-          KuGouTabBarViewScrollerNotification(event.delta.dx).dispatch(context);
-        }
-      },
-      onPointerUp: (PointerUpEvent event) {
-        setState(() {
+      child: Listener(
+        onPointerDown: (PointerDownEvent event) {
+          collectionFlag = true;
+          downPoint = event.position;
           moveDraw = false;
-        });
-      },
-      child: TabBarView(
-        physics: moveDraw ? NeverScrollableScrollPhysics() : null,
-        controller: widget.controller,
-        children: widget.children,
+        },
+        onPointerMove: (PointerMoveEvent event) {
+          if (collectionFlag && event.position.dx > downPoint.dx && widget.controller.index == 0 && scrollerNoConflict) {
+            setState(() {
+              moveDraw = true;
+            });
+            collectionFlag = false;
+          }
+          if (moveDraw ) {
+            KuGouTabBarViewScrollerNotification(event.delta.dx).dispatch(context);
+          }
+        },
+        onPointerUp: (PointerUpEvent event) {
+          setState(() {
+            moveDraw = false;
+          });
+        },
+        child: TabBarView(
+          physics: ((){
+            print("moveDraw:$moveDraw");
+            return moveDraw;
+          })() ? NeverScrollableScrollPhysics() : null,
+          controller: widget.controller,
+          children: widget.children,
+        ),
       ),
     );
   }
 }
 
+// 为什么不用ScrollerNotification 因为在Scaffold之外就接收不到了，不知道哪里被拦截了
 class KuGouTabBarViewScrollerNotification extends Notification {
 
   double dx;
