354940d1d6766412a7e4ce08ba3a64d956b5854f
cl <903736662@qq.com>
2019-02-15 15:34:43 +0800

    add: “听”界面的轮播图实现

---
+++ lib/component/view/kugou_banner.dart
@@ -0,0 +1,125 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_kugou/component/net/network_image.dart';
+
+class KuGouBanner extends StatefulWidget {
+  List<String> imageUrl;
+
+  KuGouBanner({this.imageUrl, Key key}) : super(key: key);
+
+  @override
+  _KuGouBannerState createState() => _KuGouBannerState();
+}
+
+class _KuGouBannerState extends State<KuGouBanner> {
+  double _pageContentWidth;
+  ScrollController _scrollController;
+  int pageIndex = 1;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _pageContentWidth = MediaQuery.of(context).size.width * 0.85 + 20.0;
+    if (_pageContentWidth > 0.0) {
+      _scrollController = ScrollController(
+          initialScrollOffset:
+              (3 * _pageContentWidth - MediaQuery.of(context).size.width) / 2 +
+                  (pageIndex - 1) * _pageContentWidth);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 150.0,
+          alignment: Alignment.center,
+          child: Listener(
+            onPointerUp: (PointerUpEvent event) {
+              // 屏幕跟实际内容相差的部分
+              double _diff = max(0, MediaQuery.of(context).size.width * 0.15 - 20);
+              double _offset = _scrollController.offset + _diff;
+              pageIndex = (_offset / _pageContentWidth + 0.5).toInt();
+              _scrollController.animateTo(
+                  (3 * _pageContentWidth - MediaQuery.of(context).size.width) / 2 +
+                      (pageIndex - 1) * _pageContentWidth,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.linear);
+              setState(() {});
+              if (pageIndex == 0 || pageIndex == widget.imageUrl.length + 1) {
+                _sleepJumpTo(const Duration(milliseconds: 300), pageIndex);
+              }
+            },
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              controller: _scrollController,
+              child: Row(
+                children: _bildBannerItems(),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 10.0,
+          left: 10.0,
+          right: 10.0,
+          child: _buildIndicator(),
+        ),
+      ],
+    );
+  }
+
+  Future _sleepJumpTo(Duration duration, int index) async {
+    await Future.delayed(duration);
+    if (index == 0) {
+      pageIndex = widget.imageUrl.length;
+    } else if (index == widget.imageUrl.length + 1) {
+      pageIndex = 1;
+    }
+    _scrollController.jumpTo(
+        (3 * _pageContentWidth - MediaQuery.of(context).size.width) / 2 +
+            (pageIndex - 1) * _pageContentWidth);
+    setState(() {});
+  }
+
+  List<Widget> _bildBannerItems() {
+    List<Widget> items = [];
+    items.add(_buildBannerItem(widget.imageUrl[widget.imageUrl.length - 1]));
+    items.addAll(widget.imageUrl.map((v) => _buildBannerItem(v)));
+    items.add(_buildBannerItem(widget.imageUrl[0]));
+    return items;
+  }
+
+  Widget _buildBannerItem(String imageUrl) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.85,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: MyNetworkImage(imageUrl), fit: BoxFit.fitHeight),
+            borderRadius: BorderRadiusDirectional.only(
+                topStart: Radius.circular(5.0), topEnd: Radius.circular(5.0))),
+      ),
+    );
+  }
+
+  Widget _buildIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(widget.imageUrl.length, (i) {
+        return Container(
+          height: 5.0,
+          width: 5.0,
+          margin: const EdgeInsets.only(left: 5.0, right: 5.0),
+          decoration: BoxDecoration(
+            color: i == pageIndex - 1 ? Colors.white : Colors.grey,
+            shape: BoxShape.circle
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
--- lib/view/listen/home_listen.dart
+++ lib/view/listen/home_listen.dart
@@ -1,21 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kugou/component/view/kugou_banner.dart';
 
 class Listen extends StatefulWidget {
   @override
   _ListenState createState() => _ListenState();
 }
 
-class _ListenState extends State<Listen> with AutomaticKeepAliveClientMixin{
+class _ListenState extends State<Listen> with AutomaticKeepAliveClientMixin {
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 50.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+    return Stack(
+      children: <Widget>[
+        CustomScrollView(
+          slivers: <Widget>[
+            SliverList(
+                delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: KuGouBanner(imageUrl: [
+                  "https://imgessl.kugou.com/commendpic/20190109/20190109104215314555.jpg",
+                  "https://imgessl.kugou.com/commendpic/20190109/20190109104155147376.jpg",
+                  "https://imgessl.kugou.com/commendpic/20190108/20190108190956902072.jpg",
+                  "https://imgessl.kugou.com/commendpic/20190214/20190214180525144797.jpg",
+                ]),
+              )
+            ])),
+            SliverGrid.count(
+              crossAxisCount: 3,
+              childAspectRatio: 1.3,
+              children: <Widget>[
+                Container(
+                  color: Colors.red[100],
+                ),
+                Container(
+                  color: Colors.red[200],
+                ),
+                Container(
+                  color: Colors.red[300],
+                ),
+                Container(
+                  color: Colors.red[400],
+                ),
+                Container(
+                  color: Colors.red[500],
+                ),
+                Container(
+                  color: Colors.red[600],
+                ),
+              ],
+            ),
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+              return Container(
+                height: 50.0,
+                color: Colors.green,
+              );
+            }, childCount: 20))
+          ],
         ),
-      ),
+      ],
     );
   }
 
