73af77b528c9b076a6a4b05a69ae6160e651052f
cl <903736662@qq.com>
2019-02-22 10:31:44 +0800

    opt: “听”界面中部的导航栏优化 fix: 首页的初始化界面为“听”

--- lib/view/listen/home_listen.dart
+++ lib/view/listen/home_listen.dart
@@ -28,10 +28,10 @@ class _ListenState extends State<Listen>
     _homePageBloc = BlocProvider.of<HomePageBloc>(context);
     _customScrollController = ScrollController();
     _customScrollController.addListener(() {
-      if (_homePageBloc.searchIsExpand && _customScrollController.offset > 440.0) {
+      if (_homePageBloc.searchIsExpand && _customScrollController.offset > 420.0) {
         _homePageBloc.closeSearch();
         setState(() {});
-      } else if (!_homePageBloc.searchIsExpand && _customScrollController.offset < 440.0) {
+      } else if (!_homePageBloc.searchIsExpand && _customScrollController.offset < 420.0) {
         _homePageBloc.expandSearch();
         setState(() {});
       }
@@ -43,45 +43,37 @@ class _ListenState extends State<Listen>
   Widget build(BuildContext context) {
     return DecoratedBox(
       decoration: BoxDecoration(color: Colors.white),
-      child: Stack(
-        children: <Widget>[
-          CustomScrollView(
-            controller: _customScrollController,
-            slivers: <Widget>[
-              SliverToBoxAdapter(
-                child: Container(
-                  padding: const EdgeInsets.only(top: 50.0),
-                  color: Theme.of(context).primaryColor,
-                  child: KuGouBanner(controller: _bannerController, imageUrl: [
-                    "https://imgessl.kugou.com/commendpic/20190109/20190109104215314555.jpg",
-                    "https://imgessl.kugou.com/commendpic/20190109/20190109104155147376.jpg",
-                    "https://imgessl.kugou.com/commendpic/20190108/20190108190956902072.jpg",
-                    "https://imgessl.kugou.com/commendpic/20190214/20190214180525144797.jpg",
-                  ]),
-                ),
-              ),
-              _buildMenuGrid(),
-              SliverToBoxAdapter(
-                child: _buildListTitle(),
-              ),
-              SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                        return Container(
-                          height: 50.0,
-                          color: Colors.green[(index % 9 + 1) * 100],
-                        );
-                      }, childCount: 30))
-            ],
-          ),
-          Opacity(
-            opacity: _homePageBloc.searchIsExpand ? 0.0 : 1.0,
+      child: CustomScrollView(
+        controller: _customScrollController,
+        slivers: <Widget>[
+          SliverToBoxAdapter(
             child: Container(
-              width: double.infinity,
-              color: Colors.white,
-              child: _buildListTitle(topPadding: 0.0),
+              padding: const EdgeInsets.only(top: 50.0),
+              color: Theme.of(context).primaryColor,
+              child: KuGouBanner(controller: _bannerController, imageUrl: [
+                "https://imgessl.kugou.com/commendpic/20190109/20190109104215314555.jpg",
+                "https://imgessl.kugou.com/commendpic/20190109/20190109104155147376.jpg",
+                "https://imgessl.kugou.com/commendpic/20190108/20190108190956902072.jpg",
+                "https://imgessl.kugou.com/commendpic/20190214/20190214180525144797.jpg",
+              ]),
             ),
-          )
+          ),
+          _buildMenuGrid(),
+          SliverPersistentHeader(
+            delegate: KuGouHeaderDelegate(
+                height: 40.0,
+                child: _buildListTitle()
+            ),
+            pinned: true,
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                    return Container(
+                      height: 50.0,
+                      color: Colors.green[(index % 9 + 1) * 100],
+                    );
+                  }, childCount: 30))
         ],
       ),
     );
@@ -139,9 +131,9 @@ class _ListenState extends State<Listen>
     );
   }
 
-  Widget _buildListTitle({double topPadding = 20.0}) {
+  Widget _buildListTitle() {
     return Container(
-      padding: EdgeInsets.only(top: topPadding, bottom: 3.0),
+      height: 40.0,
       decoration: BoxDecoration(color: Colors.white),
       child: TabBar(
         isScrollable: true,
@@ -177,4 +169,28 @@ class _ListenState extends State<Listen>
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class KuGouHeaderDelegate extends SliverPersistentHeaderDelegate {
+  double height;
+  Widget child;
+
+  KuGouHeaderDelegate({@required this.height, @required this.child});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => height;
+
+  @override
+  double get minExtent => height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+
 }
